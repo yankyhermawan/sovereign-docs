d11a28d07c7e40dd21399797f7123dbe11f208e1
yankyhermawan1 <yanky.hermawan@movingbytesplayground.com>
2025-07-29 20:30:00 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	preview: {
+		allowedHosts: ['sovereign-docs-production.up.railway.app/'],
+	},
 })
